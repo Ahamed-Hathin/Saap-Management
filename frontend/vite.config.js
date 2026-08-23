ab49698a44cb45cb6d297b105c6b10750e8ca6c6
Ahamed-Hathin <ahamed.hathin@example.com>
2026-08-23 14:34:50 +0530

    fix: increase workbox max file size to cache to fix vercel build

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,6 +7,9 @@ export default defineConfig({
     react(),
     VitePWA({
       registerType: 'autoUpdate',
+      workbox: {
+        maximumFileSizeToCacheInBytes: 5000000
+      },
       devOptions: {
         enabled: true
       },
